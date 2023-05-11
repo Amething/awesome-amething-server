e31ddfe0a2145a0cc000a412bd88230ac7d50190
YongWoo <aba1430@naver.com>
2023-05-11 19:08:07 +0900

    [FIX] Class Name 변경

--- src/main/kotlin/com/awesome/amething/domain/answer/Answer.kt
+++ src/main/kotlin/com/awesome/amething/domain/answer/Answer.kt
@@ -5,7 +5,7 @@ import jakarta.persistence.*
 
 @Entity
 @Table(name = "answer")
-class answer (
+class Answer (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "answer_id")
