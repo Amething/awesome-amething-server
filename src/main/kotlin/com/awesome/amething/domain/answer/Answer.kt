2e4c96084bafe5e9fdd5d48ba92eecaecf44465f
siwony <official.siwony@gmail.com>
2023-05-14 00:54:19 +0900

    import 와일드카드 제거ㅎ

--- src/main/kotlin/com/awesome/amething/domain/answer/Answer.kt
+++ src/main/kotlin/com/awesome/amething/domain/answer/Answer.kt
@@ -1,7 +1,16 @@
 package com.awesome.amething.domain.answer
 
 import com.awesome.amething.domain.question.Question
-import jakarta.persistence.*
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "answer")
--- src/main/kotlin/com/awesome/amething/domain/user/User.kt
+++ src/main/kotlin/com/awesome/amething/domain/user/User.kt
@@ -1,7 +1,17 @@
 package com.awesome.amething.domain.user
 
 import com.awesome.amething.global.enums.AuthenticatorRole
-import jakarta.persistence.*
+import jakarta.persistence.CollectionTable
+import jakarta.persistence.Column
+import jakarta.persistence.ElementCollection
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "user")
