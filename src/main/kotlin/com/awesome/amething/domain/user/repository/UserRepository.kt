bc4125c22be1e7425b058efb79dd936873a3a1d6
YongWoo <aba1430@naver.com>
2023-05-15 22:33:03 +0900

    [ADD] UserRepository

---
+++ src/main/kotlin/com/awesome/amething/domain/user/repository/UserRepository.kt
@@ -0,0 +1,8 @@
+package com.awesome.amething.domain.user.repository
+
+import com.awesome.amething.domain.user.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<User,Long>{
+   fun findAllByUsername(username : String) : User
+}
