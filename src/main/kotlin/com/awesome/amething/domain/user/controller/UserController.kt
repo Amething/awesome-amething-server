e4e694b67ec5d7d3dd8bfc332c46a0439f10da15
YongWoo <aba1430@naver.com>
2023-05-15 22:32:49 +0900

    [ADD] UserController

---
+++ src/main/kotlin/com/awesome/amething/domain/user/controller/UserController.kt
@@ -0,0 +1,21 @@
+package com.awesome.amething.domain.user.controller
+
+import com.awesome.amething.domain.user.dto.UserDto
+import com.awesome.amething.domain.user.service.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/v1/user")
+class UserController @Autowired constructor(
+  private val userService : UserService
+  ) {
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/registration")
+    fun userRegistration(@RequestBody userDto : UserDto) : ResponseEntity<HttpStatus>{
+        userService.userRegistration(userDto)
+        return ResponseEntity(HttpStatus.OK)
+    }
+}
