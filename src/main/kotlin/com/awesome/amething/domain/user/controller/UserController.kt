3c3d5d31797fd80ab61a4316189e6c143fe08e26
siwony <official.siwony@gmail.com>
2023-05-20 21:51:27 +0900

    Add user 프로필 조회 완료

--- src/main/kotlin/com/awesome/amething/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/awesome/amething/domain/user/controller/UserController.kt
@@ -1,10 +1,14 @@
 package com.awesome.amething.domain.user.controller
 
 import com.awesome.amething.domain.user.dto.UserRegisterDto
+import com.awesome.amething.domain.user.model.UserProfile
+import com.awesome.amething.domain.user.service.UserProfileService
 import com.awesome.amething.domain.user.service.UserRegistrationService
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/v1/user")
 class UserController @Autowired constructor(
     private val userRegistrationService: UserRegistrationService,
+    private val userProfileService: UserProfileService,
 ) {
     @PostMapping("/register")
     fun register(
@@ -24,4 +29,12 @@ class UserController @Autowired constructor(
         userRegistrationService.register(userRegisterDto)
         return ResponseEntity.ok().build()
     }
+
+    @GetMapping("/{username}")
+    fun findProfile(
+        @PathVariable
+        username: String,
+    ): ResponseEntity<UserProfile> =
+        ResponseEntity.ok(userProfileService.findUserProfile(username))
+
 }
---
+++ src/main/kotlin/com/awesome/amething/domain/user/service/UserProfileService.kt
@@ -0,0 +1,25 @@
+package com.awesome.amething.domain.user.service
+
+import com.awesome.amething.domain.user.model.UserProfile
+import com.awesome.amething.domain.user.repository.UserRepository
+import com.awesome.amething.global.exception.AmethingException
+import com.awesome.amething.global.exception.ErrorCode
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UserProfileService(
+    private val userRepository: UserRepository,
+) {
+    @Transactional(readOnly = true)
+    fun findUserProfile(username: String): UserProfile {
+        val user = (userRepository.findByUsername(username)
+            ?: throw AmethingException(ErrorCode.USER_NOT_FOUND))
+        return UserProfile(
+            username = user.username,
+            nickname = user.nickname,
+            bio = user.bio,
+            profilePicture = user.profilePicture
+        )
+    }
+}
