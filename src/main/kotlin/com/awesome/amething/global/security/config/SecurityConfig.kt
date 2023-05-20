83c6653eafb7b154a8bece0cb417dca2810b6e3f
siwony <official.siwony@gmail.com>
2023-05-20 17:41:18 +0900

    Add passwordEncoder bean 추가'

--- src/main/kotlin/com/awesome/amething/global/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/awesome/amething/global/security/config/SecurityConfig.kt
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
@@ -37,4 +39,6 @@ class SecurityConfig(
         http.apply(securityExceptionHandlerFilterConfig)
         return http.build()
     }
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
