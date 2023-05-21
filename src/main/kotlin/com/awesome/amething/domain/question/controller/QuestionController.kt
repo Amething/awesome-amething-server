8769cc9d8f60aca6c4e0dda751840942348e5c2e
siwony <official.siwony@gmail.com>
2023-05-21 18:04:45 +0900

    Fix bean validation 누락

--- src/main/kotlin/com/awesome/amething/domain/question/controller/QuestionController.kt
+++ src/main/kotlin/com/awesome/amething/domain/question/controller/QuestionController.kt
@@ -7,12 +7,14 @@ import com.awesome.amething.domain.question.service.QuestionQueryService
 import com.awesome.amething.global.enums.QuestionType
 import com.awesome.amething.global.exception.AmethingException
 import com.awesome.amething.global.exception.ErrorCode
+import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -39,6 +41,7 @@ class QuestionController(
     fun addQuestion(
         @PathVariable
         username: String,
+        @RequestBody @Valid
         request: QuestionSaveModel.Model,
     ): ResponseEntity<QuestionSaveModel.Result> = ResponseEntity.ok(
         questionCommandService.addQuestion(username, request),
--- src/main/kotlin/com/awesome/amething/domain/question/model/QuestionSaveModel.kt
+++ src/main/kotlin/com/awesome/amething/domain/question/model/QuestionSaveModel.kt
@@ -1,7 +1,10 @@
 package com.awesome.amething.domain.question.model
 
+import jakarta.validation.constraints.NotBlank
+
 class QuestionSaveModel {
     data class Model(
+        @field:NotBlank
         val title: String,
     )
     data class Result(
