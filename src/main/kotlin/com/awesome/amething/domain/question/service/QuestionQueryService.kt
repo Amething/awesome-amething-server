2386080702da1aa3fefe3d99545afffe91aef287
siwony <official.siwony@gmail.com>
2023-05-21 17:51:02 +0900

    QuestionQueryService에 Transactional readOnly적용

--- src/main/kotlin/com/awesome/amething/domain/question/service/QuestionQueryService.kt
+++ src/main/kotlin/com/awesome/amething/domain/question/service/QuestionQueryService.kt
@@ -4,12 +4,13 @@ import com.awesome.amething.domain.question.model.QuestionQueryModel
 import com.awesome.amething.domain.question.repository.QuestionRepository
 import com.awesome.amething.global.enums.QuestionType
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional(readOnly = true)
 class QuestionQueryService(
     private val questionRepository: QuestionRepository,
 ) {
-
     fun findQuestion(username: String, questionType: QuestionType): QuestionQueryModel {
         val questions = findQuestionByUsername(username, questionType)
         return QuestionQueryModel.from(questions)
